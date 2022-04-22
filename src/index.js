83f54067a18ecadc8a72406a6261ee97123fc26a
Mathieu LALLEMAND <2410437+lalmat@users.noreply.github.com>
2022-04-22 16:52:04 +0200

    rollback on xsrfToken & csrfToken

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ export const fetchApi = {
     const appToken = cookiesStorage.getItem('_appToken');
     if (appToken)
       headersBase.append ("Authorization", `Bearer ${atob(appToken)}`);
-
+/*
     const xsrfToken = cookiesStorage.getItem('XSRF-TOKEN');
     if (xsrfToken)
       headersBase.append('X-XSRF-TOKEN', xsrfToken);
@@ -31,7 +31,7 @@ export const fetchApi = {
     const csrfToken = document.head.querySelector('meta[name="csrf-token"]')
     if (csrfToken)
       headersBase.append('X-CSRF-TOKEN', csrfToken.content);
-
+*/
     const response = await fetch(url, {
       method: verb,
       headers: headersBase,
